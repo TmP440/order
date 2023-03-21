30f12682d1eaffff5d5e4c95016b7f8430db859a
gemcheck <74297609+gemcheck@users.noreply.github.com>
2023-03-21 21:59:32 +0300

    Add files via upload

---
+++ mytcpserver.cpp
@@ -0,0 +1,49 @@
+#include "mytcpserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+
+MyTcpServer::~MyTcpServer()
+{
+    //mTcpSocket->close();
+    mTcpServer->close();
+    server_status=0;
+}
+MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
+    mTcpServer = new QTcpServer(this);
+    connect(mTcpServer, &QTcpServer::newConnection,
+            this, &MyTcpServer::slotNewConnection);
+
+    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+        qDebug() << "server is not started";
+    } else {
+        server_status=1;
+        qDebug() << "server is started";
+    }
+}
+
+void MyTcpServer::slotNewConnection(){
+    if(server_status==1){
+        mTcpSocket = mTcpServer->nextPendingConnection();
+        mTcpSocket->write("Hello, people!!! I am echo server!\r\n");
+        connect(mTcpSocket, &QTcpSocket::readyRead,
+                this,&MyTcpServer::slotServerRead);
+        connect(mTcpSocket,&QTcpSocket::disconnected,
+                this,&MyTcpServer::slotClientDisconnected);
+    }
+}
+
+void MyTcpServer::slotServerRead(){
+    QByteArray array;
+    Qstring str;
+    while(mTcpSocket->bytesAvailable()>0){
+        array = mTcpSocket->readAll();
+        str += array;
+    }
+    array = "";
+    array.append(str);
+    mTcpSocket->write(array);
+}
+
+void MyTcpServer::slotClientDisconnected(){
+    mTcpSocket->close();
+}
---
+++ mytcpserver.h
@@ -0,0 +1,40 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+#include <QtNetwork>
+#include <QByteArray>
+#include <QDebug>
+/// <summary>
+/// 
+/// </summary>
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+
+    void slotServerRead();
+    //void slotReadClient();
+    void parsing(QString data_from_client);
+    void auth(QString log, QString pass);
+    void reg(QString log, QString pass, QString name);
+private:
+    QTcpServer * mTcpServer;
+    QTcpSocket * mTcpSocket;
+    int server_status;
+};
+#endif // MYTCPSERVER_H
+
+
+
+
+
+
+
