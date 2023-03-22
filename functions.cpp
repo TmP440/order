426b466c9256b73ff849122fd51f123bae6a157c
TmP440 <kerambit12323@mail.ru>
2023-03-22 11:47:55 +0300

    Function names have been changed

--- functions.cpp
+++ functions.cpp
@@ -1,10 +1,12 @@
 #include "functions.h"
 
 
-QByteArray auth(QString login, QString password){
+QByteArray log_in(QString login, QString password){
     return QByteArray("Авторизация!.!\r\n");
 }
 
-QByteArray reg(QString login, QString password){
-    return QByteArray("Регситрация!.!\r\n");
+QByteArray log_out(QString login, QString password){
+    return QByteArray("Выход из системы!.!\r\n");
 }
+
+QByteArray
--- functions.h
+++ functions.h
@@ -6,16 +6,16 @@
 #include <QByteArray>
 #include <QMap>
 #include <QStringList>
-/// \brief auth отвечавет за авторизацию (вход в систему)
+/// \brief log_in отвечавет за вход
 /// \param QString login, QString password
 /// \return QByteArray response
 ///
-QByteArray auth(QString login, QString password);
+QByteArray log_in(QString login, QString password);
 
-/// \brief reg отвечавет за регистрацию в системе
+/// \brief log_out отвечавет за выход из системы
 /// \param QString login, QString password
 /// \return QByteArray response
 ///
-QByteArray reg(QString login, QString password);
+QByteArray log_out(QString login, QString password);
 
 #endif // FUNCTIONS_H
