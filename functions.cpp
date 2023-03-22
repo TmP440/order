e3c762eff62c11df402a94d22d56fd01a0fbda30
TmP440 <kerambit12323@mail.ru>
2023-03-22 12:17:03 +0300

    Init commit

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include "functions.h"
+
+
+QByteArray log_in(QString login, QString password){
+    return QByteArray("Авторизация!.!\r\n");
+}
+
+QByteArray log_out(QString login, QString password){
+    return QByteArray("Выход из системы!.!\r\n");
+}
+
+QByteArray reg(QString login, QString password){
+    return QByteArray("Регистрация!.!\r\n");
+}
