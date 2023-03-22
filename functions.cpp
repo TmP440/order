d58b85b5565fbd6a9f4cb398d388dc057265483c
TmP440 <kerambit12323@mail.ru>
2023-03-22 14:21:23 +0300

    Добавлены новые функции для сервера

--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,6 @@
 #include "functions.h"
+#include <iostream>
+#include <Windows.h>
 
 
 QByteArray log_in(QString login, QString password){
@@ -13,4 +15,28 @@ QByteArray reg(QString login, QString password){
     return QByteArray("Регистрация!.!\r\n");
 }
 
+QByteArray invalid_request(){
+    return QByteArray("Ошибка! Такой команды нет\n или неверное кол-во коканд");
+}
 
+QByteArray change_password(QString old_pass, QString new_pass_1, QString new_pass_2){
+    return QByteArray("Пароль изменен!");
+}
+
+QByteArray start_task(QString){
+    return QByteArray("Ваша задача...\r\n");
+}
+
+QByteArray check_task(int, QString){
+    return QByteArray("Проверка задачи...\r\n");
+}
+
+void parsing(QString command){
+    QStringList parts = command.split(" ");
+    std::cout << &parts;
+}
+
+int main() {
+    parsing("Привет как дела");
+    return 0;
+}
